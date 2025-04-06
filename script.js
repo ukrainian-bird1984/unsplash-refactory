80ef425ab5cd817975cd5039167ecbfe3a55a032
ukrainian-bird1984 <nadiiabasaraba84@gmail.com>
2025-04-06 19:26:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const ACCESS_KEY = 'pCf_FzDA5s8yR4Hm8Zwli4VDOL1mqn_pjt7PcQgG1Do'; 
 
+//receive imagine function//
 async function searchImages() {
   const query = document.getElementById ('searchInput').value;
-  const url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${ACCESS_KEY}&per_page=10`;
+  const url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${ACCESS_KEY}&per_page=20`;
 
   try {
     const response = await axios.get(url);
@@ -23,6 +24,7 @@ async function searchImages() {
   }
 }
 
+//slideshow function//
 function startSlideshow() {
   const images = document.querySelectorAll('#slideshow img');
   let index = 0;
